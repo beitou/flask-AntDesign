aef3a5dfc79382b18863e3fcabcb1261727a03c7
陈信宏 <wanwanling@xiaobangtouzi.com>
2021-05-31 16:59:43 +0800

    flask+AntDesign

---
+++ quality/app/treasure/models.py
@@ -0,0 +1,31 @@
+
+import datetime
+
+from app import db
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
+from sqlalchemy import DateTime
+from app.models import BaseModel
+
+
+class Treasure(BaseModel):
+    __tablename__ = "tbl_treasure_data"
+
+    id = Column(Integer, primary_key=True, comment="ID")
+    name = Column(String(32), nullable=False, comment="App名称")
+    version_name = Column(String(32), nullable=False, comment="App版本名")
+    version = Column(String(32), nullable=False, comment="App版本")
+    branch = Column(String(32), nullable=False, comment="分支名")
+    platform = Column(String(32), nullable=False, comment="平台[iOS|Android]")
+    type = Column(String(32), nullable=False, comment="分支名")
+    commit_id = Column(String(40), nullable=False, comment="commit id")
+    submitter = Column(String(32), nullable=False, comment="分支名")
+    url = Column(String(2048), nullable=False, comment="下载链接")
+    image = Column(String(2048), nullable=False, comment="二维码图片地址")
+    created = Column(DateTime, default=datetime.datetime.now, comment="创建时间")
+
+
+if __name__ == '__main__':
+    from app import app
+    db.create_all(app=app)
---
+++ quality/fe/layouts/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <a-row>
+      <a-col :span="21">
+        <sider-menu />
+      </a-col>
+      <a-col :span="3">
+        <user-menu />
+      </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script>
+import SiderMenu from './SiderMenu'
+import UserMenu from '@/components/GlobalHeader/UserMenu.vue'
+
+export default {
+  components: {
+    SiderMenu,
+    UserMenu,
+  },
+  methods: {
+    localeChange({ key }) {
+      this.$router.push({ query: { ...this.$route.query, locale: key } })
+      this.$i18n.locale = key
+    },
+  },
+}
+</script>
